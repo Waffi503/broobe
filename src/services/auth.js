93d591ce1e040a98b2e26089673c7509c1e21ab8
Carlos Brandan <carlosbrandan503@gmail.com>
2023-11-29 00:27:46 -0300

    fusion signup and register

--- src/services/auth.js
+++ src/services/auth.js
@@ -21,3 +21,22 @@ export const login = async (body) => {
         };          
     }
 }
+
+export const signup = async (body) => {
+    
+    try{
+        const response = await post('users', body);
+        return  {
+            status: response.status,
+            message: response.message,
+            data: response.data,
+            error:''
+        };
+    } catch (error) {
+        return {
+            status: 500,
+            message: 'Error en el servidor',
+            error: 'Fail to create user'
+        };          
+    }
+}
