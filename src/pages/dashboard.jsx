8a1a2eb3c83e0db6fa1bbf32b690386006af2558
Carlos Brandan <carlosbrandan503@gmail.com>
2023-11-29 00:18:14 -0300

    dashboard page with loading

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import { Input } from "@/components/ui/input"
 import { useForm  } from "react-hook-form"
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -6,16 +6,23 @@ import * as z from 'zod';
 import { Button } from '@/components/ui/button';
 import { login } from '@/services/login';
 import { useNavigate } from 'react-router-dom';
+import { getIssues } from '@/services/issues';
+import useFetch from '@/hooks/useFetch';
+import ListIssues from '@/components/dashboard/listIssues';
 
 
 export default function Dashboard() {
-
-
+    const { data, error, isLoading } = useFetch(getIssues);
     return (
       <div className="flex justify-center mt-5">
-        <div  className="min-w-[300px]">
+        <div>
           <h1 className="text-center font-bold text-3xl">Dashboard Issues</h1>
-
+            <p className='text-center text-red-500 text-sm'>{error}</p>
+            {isLoading ? 
+                <p>Loading...</p> 
+            : 
+                <ListIssues issues={data} />
+            }
         </div>
       </div>
     );
