abff8ae8405ea0b942afe834be84762dc9db685b
Carlos Brandan <carlosbrandan503@gmail.com>
2023-11-30 00:39:11 -0300

    fix bug in deploy

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { AuthContext } from '@/contexts/AuthContext';
+import { AuthContext } from '@/contexts/authcontext';
 
 export default function useAuth() {
   const {
